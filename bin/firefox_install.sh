ff3bc67d8eefd5ac933e0bee3596c6a2546355be
Zatsune No Mokou <7016954+ZatsuneNoMokou@users.noreply.github.com>
2023-02-10 13:21:35 +0100

    Create firefox_install.sh

---
+++ bin/firefox_install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -o firefox-dev.tar.bz2 https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US && tar -jxvf firefox-dev.tar.bz2 && rm firefox-dev.tar.bz2 && sudo rm -rf /opt/firefox && sudo mv firefox /opt
