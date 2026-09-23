53fb355582a333d217d6fef4af7d00ed62f8fc03
Zatsune Sakurako <ZatsuneSakurako@users.noreply.github.com>
2026-09-23 19:36:24 +0200

    stirling_pdf.sh

--- bin/stirling_pdf.sh
+++ bin/stirling_pdf.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 containerName="stirling-pdf"
-if [ "$(docker inspect -f '{{.State.Running}}' "$containerName")" == "true" ]; then
-	echo "Stirling PDF already running"
-else
-	docker run --rm --name "$containerName" -p 8080:8080 \
-		-e SECURITY_ENABLELOGIN=false \
-		-e SYSTEM_DEFAULTLOCALE=en-GB \
-		docker.stirlingpdf.com/stirlingtools/stirling-pdf:latest
-fi
+docker run --rm --name "$containerName" -p 8080:8080 \
+	-e SECURITY_ENABLELOGIN=false \
+	-e SYSTEM_DEFAULTLOCALE=en-GB \
+	docker.stirlingpdf.com/stirlingtools/stirling-pdf:latest
